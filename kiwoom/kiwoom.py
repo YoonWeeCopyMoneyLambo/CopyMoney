d0d8e785fcda25693a3cc3fefc905c018f8ac8cc
sprite316 <star854@nate.com>
2021-08-22 10:09:31 +0900

    test

---
+++ kiwoom/kiwoom.py
@@ -0,0 +1,74 @@
+from PyQt5.QAxContainer import *
+from PyQt5.QtCore import *
+from config.errorCode import *
+
+class Kiwoom(QAxWidget):
+    def __init__(self):
+        super().__init__()
+
+        print("Kiwoom 클래스 입니다")
+
+        ###### eventloop 모음
+        self.login_event_loop = None
+        #############################
+
+        ###### 변수 모음
+        self.account_num = None
+        #############################
+
+        self.get_ocx_instance()
+        self.event_slots()
+        self.signal_login_commConnect()
+        self.get_account_info()
+        self.detail_account_info() # 예수금 가져오는 것!
+
+    def get_ocx_instance(self):
+        self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
+
+    def event_slots(self):
+        self.OnEventConnect.connect(self.login_slot)
+        self.OnReceiveTrData.connect(self.trdata_slot)
+
+    def signal_login_commConnect(self):
+        self.dynamicCall("CommConnect()")
+
+        self.login_event_loop = QEventLoop()
+        self.login_event_loop.exec_()
+
+    def login_slot(self, errCode):
+        print(errors(errCode))
+        self.login_event_loop.exit()
+
+    def get_account_info(self):
+        account_list = self.dynamicCall("GetLogininfo(String)", "ACCNO")
+        self.account_num = account_list.split(';')[0]
+        print("나의 보유 계좌번호 %s" % self.account_num) #8005704011
+
+    def detail_account_info(self):
+        print("예수금 요청하는 부분")
+
+        self.dynamicCall("SetInputValue(String, String)", "계좌번호", self.account_num)
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호", "0000")
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호입력매체구문", "00")
+        self.dynamicCall("SetInputValue(String, String)", "조회구문", "2")
+        self.dynamicCall("CommRqData(String, String, int, String)", "예수금상세현황요청", "opw00001", "0", "2000")
+
+    def trdata_slot(self, sScrNo, sRQName, sTrCode, sRecordName, sPrevNext,):
+        '''
+        tr요청을 받는 구역이다! 슬롯이다!
+        :param sScrNo: 스크린번호
+        :param sRQName: 내가 요청했을 때 지은 이름
+        :param sTrCode: 요청id, tr코드
+        :param sRecordName: 사용 안함
+        :param sPrevNext: 다음 페이지가 있는지
+        :return: 
+        '''
+
+        if sRQName == "예수금상세현황요청":
+            deposit = self.dynamicCall("GetCommData(String, String, int, String)", sTrCode, sRQName, 0, "예수금")
+            print("예수금 %s" % type(deposit))
+            print("예수금 형변환 %s" % int(deposit))
+
+            ok_deposit = deposit = self.dynamicCall("GetCommData(String, String, int, String)", sTrCode, sRQName, 0, "출금가능금액")
+            print("출금가능금액 %s" % ok_deposit)
+            print("출금가능금액 형변환 %s" % int(ok_deposit))
---
+++ ui/ui.py
@@ -0,0 +1,14 @@
+from kiwoom.kiwoom import *
+
+import sys
+from PyQt5.QtWidgets import *
+
+class Ui_class():
+    def __init__(self):
+        print("Ui_class 입니다")
+
+        self.app = QApplication(sys.argv)
+
+        self.kiwoom = Kiwoom()
+
+        self.app.exec_()  # 프로그램 종료되지 않게 유지
