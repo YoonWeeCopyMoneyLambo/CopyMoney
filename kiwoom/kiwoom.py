424c56de44dc0d7022367946be71360907110a23
sprite316 <star854@nate.com>
2021-08-29 02:47:32 +0900

    test

--- kiwoom/kiwoom.py
+++ kiwoom/kiwoom.py
@@ -10,17 +10,26 @@ class Kiwoom(QAxWidget):
 
         ###### eventloop 모음
         self.login_event_loop = None
+        self.detail_account_info_event_loop = None
+        self.detail_account_info_event_loop_2 = None
         #############################
 
         ###### 변수 모음
         self.account_num = None
+        self.account_stock_dict = {}
         #############################
+
+        ###### 계좌 관련 변수
+        self.use_money = 0
+        self.use_money_percent = 0.5
+
 
         self.get_ocx_instance()
         self.event_slots()
         self.signal_login_commConnect()
         self.get_account_info()
         self.detail_account_info() # 예수금 가져오는 것!
+        self.detail_account_mystock() # 계좌평가 잔고내역 요청
 
     def get_ocx_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
@@ -42,7 +51,7 @@ class Kiwoom(QAxWidget):
     def get_account_info(self):
         account_list = self.dynamicCall("GetLogininfo(String)", "ACCNO")
         self.account_num = account_list.split(';')[0]
-        print("나의 보유 계좌번호 %s" % self.account_num) #8005704011
+        print("나의 보유 계좌번호 %s" % self.account_num) #내계좌번호 8005704011
 
     def detail_account_info(self):
         print("예수금 요청하는 부분")
@@ -52,6 +61,20 @@ class Kiwoom(QAxWidget):
         self.dynamicCall("SetInputValue(String, String)", "비밀번호입력매체구문", "00")
         self.dynamicCall("SetInputValue(String, String)", "조회구문", "2")
         self.dynamicCall("CommRqData(String, String, int, String)", "예수금상세현황요청", "opw00001", "0", "2000")
+
+        self.detail_account_info_event_loop = QEventLoop()
+        self.detail_account_info_event_loop.exec_()
+
+    def detail_account_mystock(self, sPrevNext="0"):
+        print("계좌평가 잔고내역 요청")
+        self.dynamicCall("SetInputValue(String, String)", "계좌번호", self.account_num)
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호", "0000")
+        self.dynamicCall("SetInputValue(String, String)", "비밀번호입력매체구문", "00")
+        self.dynamicCall("SetInputValue(String, String)", "조회구문", "2")
+        self.dynamicCall("CommRqData(String, String, int, String)", "계좌평가잔고내역", "opw00018", sPrevNext, "2000")
+
+        self.detail_account_info_event_loop_2 = QEventLoop()
+        self.detail_account_info_event_loop_2.exec_()
 
     def trdata_slot(self, sScrNo, sRQName, sTrCode, sRecordName, sPrevNext,):
         '''
@@ -69,6 +92,65 @@ class Kiwoom(QAxWidget):
             print("예수금 %s" % type(deposit))
             print("예수금 형변환 %s" % int(deposit))
 
+            self.use_money = int(deposit) * self.use_money_percent ### 전체돈의 use_money_percet 만큼만
+            self.use_money = self.use_money / 4  ### 그 중에서도 1/4만
+
             ok_deposit = deposit = self.dynamicCall("GetCommData(String, String, int, String)", sTrCode, sRQName, 0, "출금가능금액")
             print("출금가능금액 %s" % ok_deposit)
             print("출금가능금액 형변환 %s" % int(ok_deposit))
+
+            self.detail_account_info_event_loop.exit()
+        
+        if sRQName == "계좌평가잔고내역":
+            total_buy_money = self.dynamicCall("GetCommData(String, String, int, String)", sTrCode, sRQName, 0, "총매입금액")
+            total_buy_money_result = int(total_buy_money)
+
+            print("총매입금액 %s" % total_buy_money_result)
+
+            total_profit_loss_rate = self.dynamicCall("GetCommData(String, String, int, String)", sTrCode, sRQName, 0, "총수익률(%)")
+            total_profit_loss_rate_result = float(total_profit_loss_rate)
+
+            print("총수익률(%%) : %s" % total_profit_loss_rate_result)  #### 44강 마지막부분에 왜 %%했는지 나옴
+
+            rows = self.dynamicCall("GetRepeatCnt(QString, QString", sTrCode, sRQName)
+            cnt = 0
+            for i in range(rows):
+                code = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "종목번호")
+                code = code.strip()[1:] ### 종목코드앞에 붙은 A를 지워준다
+
+                code_nm = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "종목명")
+                stock_quantity = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "보유수량")
+                buy_price = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "매입가")
+                learn_rate = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "수익률(%)")
+                current_price = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "현재가")
+                total_chegual_price = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "매입금액")
+                possible_quantity = self.dynamicCall("GetCommData(QString, QString, int, QString)", sTrCode, sRQName, i, "매매가능수량")
+
+                if code in self.account_stock_dict:
+                    pass
+                else:
+                    self.account_stock_dict.update({code:{}})
+
+                code_nm = code_nm.strip()
+                stock_quantity = int(stock_quantity.strip())
+                buy_price = int(buy_price.strip())
+                learn_rate = float(learn_rate.strip())
+                current_price = int(current_price.strip())
+                total_chegual_price = int(total_chegual_price.strip())
+                possible_quantity = int(possible_quantity.strip())
+
+                self.account_stock_dict[code].update({"종목명": code_nm})
+                self.account_stock_dict[code].update({"보유수량": stock_quantity})
+                self.account_stock_dict[code].update({"매입가": buy_price})
+                self.account_stock_dict[code].update({"수익률(%)": learn_rate})
+                self.account_stock_dict[code].update({"현재가": current_price})
+                self.account_stock_dict[code].update({"매입금액": total_chegual_price})
+                self.account_stock_dict[code].update({"매매가능수량": possible_quantity})
+
+                cnt += 1
+
+            print("계좌에 가지고 있는 종목 %s" % cnt)
+
+            self.detail_account_info_event_loop_2.exit()
+            
+            ##### 46강 7:39부터 이어서 치기
